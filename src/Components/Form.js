76e570dd73b90629bdc7a367f3528991bcb6dca7
Promise Temitope <oluwatobilobatemi05@gmail.com>
2023-01-03 19:01:00 +0200

    Finished form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Form = () => {
   return (
     <div className="min-h-90vh bg-green px-4">
-      <div className="max-w-1/2">
+      <div className="max-w-1/2 w-1/3">
         <h3 className="text-[4rem]">Get in touch</h3>
         <p className="text-xl">
           <span>
@@ -13,67 +13,79 @@ const Form = () => {
         </p>
       </div>
       <form
-        className="max-w-1/2 pt-4"
+        id="GetInTouch"
+        className="max-w-1/2 w-1/3 pt-4"
         action="mailto:contact@yourdomain.com"
         method="post"
         encType="text/plain"
         name="Contact_Us"
       >
-        <ul>
+        <ul className="max-w-[30rem] space-y-2">
+          <h3>
+            <span className="after:content-['*'] after:ml-0.5 after:text-warning-red block text-2xl font-semibold">
+              Name
+            </span>
+          </h3>
           <li>
-            <h3>
-              <span className="after:content-['*'] after:ml-0.5 after:text-warning-red block text-2xl font-semibold">
-                Name
-              </span>
-            </h3>
-            <section className="flex flex-row space-x-4">
-              <div className="flex flex-col">
-                <label for="fname" className="after:content-['*'] after:ml-0.5 after:text-warning-red ">
+            <section className="flex flex-row w-full flex-wrap space-y-1">
+              <div className="flex flex-col w-1/2 max-w-[50%] min-w-[40%]">
+                <label for="fname">
                   First Name
+                  <span className="after:content-['*'] after:ml-0.5 after:text-warning-red "></span>
+                  <input
+                    className="p-2 rounded-lg max-w-[95%] min-w-[90%]"
+                    id="fname"
+                    name="fname"
+                    required
+                  />
                 </label>
-                <input
-                  id="fname"
-                  name="fname"
-                  required
-                  className="p-2 rounded-lg"
-                />
               </div>
-              <div className="flex flex-col">
-                <label for="lname" className="after:content-['*'] after:ml-0.5 after:text-warning-red ">Last Name</label>
-                <input
-                  id="lname"
-                  name="lname"
-                  required
-                  className="p-2 rounded-lg"
-                />
+              <div className="flex flex-col w-1/2 max-w-[50%] min-w-[40%]">
+                <label for="lname">
+                  Last Name
+                  <span className="after:content-['*'] after:ml-0.5 after:text-warning-red "></span>
+                  <input
+                    className="p-2 rounded-lg max-w-[95%] min-w-[90%]"
+                    id="lname"
+                    name="lname"
+                    required
+                  />
+                </label>
               </div>
-            </section>
-            <section className="flex flex-row space-x-4">
-              <div className="flex flex-col">
-                <label for="email" className="after:content-['*'] after:ml-0.5 after:text-warning-red ">
+              <div className="flex flex-col w-1/2 max-w-[50%] min-w-[40%]">
+                <label for="mail">
                   E-mail
+                  <span className="after:content-['*'] after:ml-0.5 after:text-warning-red "></span>
+                  <input
+                    className="p-2 rounded-lg max-w-[95%] min-w-[90%]"
+                    id="mail"
+                    name="mail"
+                    required
+                  />
                 </label>
-                <input
-                  id="mail"
-                  name="mail"
-                  required
-                  className="p-2 rounded-lg"
-                />
               </div>
-              <div className="flex flex-col">
-                <label for="tel">Tel</label>
-                <input
-                  id="tel"
-                  name="tel"
-                  required
-                  className="p-2 rounded-lg"
-                />
+              <div className="flex flex-col w-1/2 max-w-[50%] min-w-[40%]">
+                <label for="tel">
+                  Telephone
+                  <input
+                    id="tel"
+                    name="tel"
+                    className="p-2 rounded-lg max-w-[95%] min-w-[90%]"
+                  />
+                </label>
               </div>
             </section>
+          </li>
+          <li>
             <div>
-                <textarea rols>
-
-                </textarea>
+              <textarea
+                form_id="GetInTouch"
+                className="p-2 rounded-lg max-w-[97.5%] w-full resize-none scroll-m-0"
+                placeholder="Send us a message!"
+                required
+                wrap="hard"
+                cols="3"
+              ></textarea>
             </div>
           </li>
         </ul>
